41268a5592482f1854426c53cff8317d30e96454
Lindany <mabasolindaniricardo@gmail.com>
2020-11-15 20:04:40 +0200

    add all

--- assets/fcf.config.php
+++ assets/fcf.config.php
@@ -62,7 +62,7 @@ define('EMAIL_OUT_FROM_NAME', '');
 // *************
 // EMAIL MESSAGE
 // *************
-define('EMAIL_TO', 'info@gazellecreecktechnologies.co.za');
+define('EMAIL_TO', 'lindani@gazellecreecktechnologies.co.za');
 define('EMAIL_TO_NAME', 'gazelle creek technologies');
 
 define('EMAIL_TO_CC', 'mabasolindanircardo@gmail.com');
@@ -71,7 +71,7 @@ define('EMAIL_TO_CC_NAME', 'Lindani');
 define('EMAIL_TO_BCC', '');
 define('EMAIL_TO_BCC_NAME', '');
 
-define('EMAIL_FROM', 'info@gazellecreektechnologies.co.za');
+define('EMAIL_FROM', 'lindani@gazellecreektechnologies.co.za');
 define('EMAIL_FROM_NAME', '');
 
 define('EMAIL_REPLY_TO', 'FIELD:Email');
@@ -87,7 +87,7 @@ define('EMAIL_SUBJECT_AFTER', '');
 // EMAIL TRANSPORT
 // ***************
 define('USE_SMTP', 'YES'); // YES or NO
-define('SMTP_HOST', 'mail.partyshoppe.co.za');
+define('SMTP_HOST', 'mail.gazellecreecktechnologies.co.za');
 define('SMTP_USER', 'info@gazellecreecktechnologies.co.za');
 define('SMTP_PASS', 'info@gazellecreecktechnologies.co.za');
 define('SMTP_AUTH', '');
